c1295f7c2dfbb71006cd5a775b10c63c33f08ece
ABDELHAFID KHALIL <abdelhafidkhalil4@gmail.com>
2024-11-24 15:10:41 +0100

    deploy the database

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
 app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
-
+app.debug = True
 mysql = MySQL(app)
 
 @app.route('/')
@@ -116,4 +116,4 @@ def index_page():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
